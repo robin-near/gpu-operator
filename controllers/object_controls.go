c59fc686ced41241ea38d8d84f8ddf92579a23f3
robin-near <robin@nearone.org>
2026-01-06 22:49:14 -0800

    bump

--- controllers/object_controls.go
+++ controllers/object_controls.go
@@ -2786,9 +2786,6 @@ func transformDriverManagerInitContainer(obj *appsv1.DaemonSet, driverManagerSpe
 		addPullSecrets(&obj.Spec.Template.Spec, driverManagerSpec.ImagePullSecrets)
 	}
 
-	container2 := findContainerByName(obj.Spec.Template.Spec.InitContainers, "setup-udev-rules")
-	container2.Image = managerImage
-
 	return nil
 }
 
